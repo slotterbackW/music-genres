daa5ce2b939eaaf9013f281fc1e8b2cb5858cd15
Will Slotterback <will2337@gmail.com>
2018-12-05 14:19:19 -0500

    Add final version of slides

--- todds_munging.py
+++
@@ -1,8 +0,0 @@
-#Top Level Genre's
-#Notes
-# Genres are all lower case
-
-#list of genre's
-
-# pulled genre categories from billboard charts found here: https://www.billboard.com/charts
-#combined genres in 'soul' section based on similar project here: http://www.silkandsilicon.com/billboard-genres
